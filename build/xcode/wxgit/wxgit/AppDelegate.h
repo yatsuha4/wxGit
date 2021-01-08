9aca70667a443a43f8cb0e13726bbc3503223050
YASUI Kentarow <yasuikentarow@gmail.com>
2021-01-08 13:03:27 +0900

    Xcode準備

---
+++ build/xcode/wxgit/wxgit/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  wxgit
+//
+//  Created by YASUI Kentarow on 2021/01/08.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ build/xcode/wxgit/wxgit/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  wxgit
+//
+//  Created by YASUI Kentarow on 2021/01/08.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
