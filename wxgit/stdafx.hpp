8d2e6c24c505c025f441d60442141d57b273b468
YASUI Kentarow <yasuikentarow@gmail.com>
2021-01-09 10:08:44 +0900

    wxWidgetsでアプリの雛形

---
+++ wxgit/stdafx.hpp
@@ -0,0 +1,40 @@
+﻿/***********************************************************************//**
+	@file
+***************************************************************************/
+#include <vector>
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+#define _FILE_OFFSET_BITS 64
+#define __WXMAC__
+#define __WXOSX__
+#define __WXOSX_COCOA__
+#include "wx/wx.h"
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+#define WXGIT_DECLARE_CLASS(klass)                                      \
+  class klass;                                                          \
+  using klass##Ptr = std::shared_ptr<klass>;                            \
+  using Const##klass##Ptr = std::shared_ptr<const klass>;               \
+
+#define WXGIT_SETTER(name, var)                                         \
+  auto set##name(const decltype(var)& value) -> decltype(*this)& {      \
+    var = value;                                                        \
+    return *this;                                                       \
+  }
+
+#define WXGIT_GETTER(name, var)                 \
+  const decltype(var)& get##name() const {      \
+    return var;                                 \
+  }
+/***********************************************************************//**
+	@brief 
+***************************************************************************/
+namespace wxgit {
+WXGIT_DECLARE_CLASS(Application);
+WXGIT_DECLARE_CLASS(MainFrame);
+}
+/***********************************************************************//**
+	$Id$
+***************************************************************************/
