dab0d514617cdc6b88b74c6eff2c3fcc97a7be80
Yasui Kentarow <yasuikentarow@gmail.com>
2023-06-01 04:10:15 +0900

    VS2019を指定

--- external/build/vs2019/libgit2.bat
+++ external/build/vs2019/libgit2.bat
@@ -3,7 +3,7 @@ set CURDIR=%~dp0
 set TMPDIR=%CURDIR%build_libgit2
 set DSTDIR=%CURDIR%libgit2
 
-rmdir /s /q "%DSTDIR"
+rmdir /s /q "%DSTDIR%"
 mkdir "%TMPDIR%"
 pushd "%TMPDIR%"
 cmake "%CURDIR%..\..\libgit2" -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX="%DSTDIR%" -DBUILD_SHARED_LIBS=off
