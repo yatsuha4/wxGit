3a3bd483a75fed892981e9384bd97407d08e114f
Yasui Kentarow <yasuikentarow@gmail.com>
2023-06-03 03:48:08 +0900

    GitHubActionsでインストーラーを作成

--- build/vs2022/build.bat
+++ build/vs2022/build.bat
@@ -1,5 +1,3 @@
 setlocal
 set CURDIR=%~dp0
-call "%CURDIR%..\..\wxEditor\external\build\vs2022\wxWidgets.bat"
-call "%CURDIR%..\..\external\build\vs2022\libgit2.bat"
-MSBuild "%CURDIR%wxGit.sln" /t:Build /p:Configuration=Release;Platform=x64
+devenv.com "%CURDIR%wxGit.sln" /Project wxGitSetup\wxGitSetup.vdproj /Rebuild Release
