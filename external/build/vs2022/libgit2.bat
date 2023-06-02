e97c23600b7cd2f36f3673d2e7c080bfbd335a68
Yasui Kentarow <yasuikentarow@gmail.com>
2023-06-03 08:04:39 +0900

    LNK4098の警告を修正

--- external/build/vs2022/libgit2.bat
+++ external/build/vs2022/libgit2.bat
@@ -6,7 +6,7 @@ set DSTDIR=%CURDIR%libgit2
 rmdir /s /q "%DSTDIR%"
 mkdir "%TMPDIR%"
 pushd "%TMPDIR%"
-cmake "%CURDIR%..\..\libgit2" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX="%DSTDIR%" -DBUILD_SHARED_LIBS=off
+cmake "%CURDIR%..\..\libgit2" -G "Visual Studio 17 2022" -A x64 -DCMAKE_INSTALL_PREFIX="%DSTDIR%" -DBUILD_SHARED_LIBS=OFF -DSTATIC_CRT=OFF
 cmake --build . --config Release --target install
 popd
 rmdir /s /q "%TMPDIR%"
